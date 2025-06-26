88102b2364c3ebb223a42accf8d317bdefcaa7ed
Ronald Bradford <ronald.bradford@gmail.com>
2025-06-26 10:40:53 -0400

    Move images to orig for next iteration

--- www/data.js
+++ www/data.js
@@ -1,816 +1,816 @@
 export const imageData = [
   {
-    "image": "05dec2007-X5.jpg",
+    "image": "orig/05dec2007-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "07nov2007-X4.jpg",
+    "image": "orig/07nov2007-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "09nov2007-X4.jpg",
+    "image": "orig/09nov2007-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "15dec2007-X3.jpg",
+    "image": "orig/15dec2007-X3.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "16nov2007-X5.jpg",
+    "image": "orig/16nov2007-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "26dec2007-X4.jpg",
+    "image": "orig/26dec2007-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "3066_181123995330_536710330_6556286_7530464_n.jpg",
+    "image": "orig/3066_181123995330_536710330_6556286_7530464_n.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "3066_181124470330_536710330_6556306_1095229_n.jpg",
+    "image": "orig/3066_181124470330_536710330_6556306_1095229_n.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "3066_181125525330_536710330_6556340_4725250_n.jpg",
+    "image": "orig/3066_181125525330_536710330_6556340_4725250_n.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "3066_181125650330_536710330_6556345_3431991_n.jpg",
+    "image": "orig/3066_181125650330_536710330_6556345_3431991_n.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "DSC00564-2-X3.jpg",
+    "image": "orig/DSC00564-2-X3.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "Empire1-X4.jpg",
+    "image": "orig/Empire1-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0005-2-X4.jpg",
+    "image": "orig/IMG_0005-2-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0018-X4.jpg",
+    "image": "orig/IMG_0018-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0021.jpg",
+    "image": "orig/IMG_0021.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0034-X5.jpg",
+    "image": "orig/IMG_0034-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0045-X4.jpg",
+    "image": "orig/IMG_0045-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0054.jpg",
+    "image": "orig/IMG_0054.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0098-1.jpg",
+    "image": "orig/IMG_0098-1.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0128-1.jpg",
+    "image": "orig/IMG_0128-1.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0132-1.jpg",
+    "image": "orig/IMG_0132-1.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0142.jpg",
+    "image": "orig/IMG_0142.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0154.jpg",
+    "image": "orig/IMG_0154.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_0410-X4.jpg",
+    "image": "orig/IMG_0410-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_1563-XL.jpg",
+    "image": "orig/IMG_1563-XL.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_1592-XL.jpg",
+    "image": "orig/IMG_1592-XL.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_1608-XL.jpg",
+    "image": "orig/IMG_1608-XL.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3096-X4.jpg",
+    "image": "orig/IMG_3096-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3222-X5.jpg",
+    "image": "orig/IMG_3222-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3397-X5.jpg",
+    "image": "orig/IMG_3397-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3448-X5.jpg",
+    "image": "orig/IMG_3448-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3470-3-X5.jpg",
+    "image": "orig/IMG_3470-3-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3599-X4.jpg",
+    "image": "orig/IMG_3599-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3628.jpg",
+    "image": "orig/IMG_3628.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3639-X5.jpg",
+    "image": "orig/IMG_3639-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3750-X5.jpg",
+    "image": "orig/IMG_3750-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3773-X4.jpg",
+    "image": "orig/IMG_3773-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3917-X5.jpg",
+    "image": "orig/IMG_3917-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_3930-X5.jpg",
+    "image": "orig/IMG_3930-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_4222-X4.jpg",
+    "image": "orig/IMG_4222-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_4237-X4.jpg",
+    "image": "orig/IMG_4237-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_4907-X3.jpg",
+    "image": "orig/IMG_4907-X3.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5273.jpg",
+    "image": "orig/IMG_5273.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5295-Edit-Edit-2.jpg",
+    "image": "orig/IMG_5295-Edit-Edit-2.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5328-X5.jpg",
+    "image": "orig/IMG_5328-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5506-X5.jpg",
+    "image": "orig/IMG_5506-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5537-X5.jpg",
+    "image": "orig/IMG_5537-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_5980-X4.jpg",
+    "image": "orig/IMG_5980-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6188.jpg",
+    "image": "orig/IMG_6188.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6194-X5.jpg",
+    "image": "orig/IMG_6194-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6384-X5.jpg",
+    "image": "orig/IMG_6384-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6430-X5.jpg",
+    "image": "orig/IMG_6430-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6446_MV.jpg",
+    "image": "orig/IMG_6446_MV.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6495.jpg",
+    "image": "orig/IMG_6495.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6501-X4.jpg",
+    "image": "orig/IMG_6501-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6513-X5.jpg",
+    "image": "orig/IMG_6513-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6527-X5.jpg",
+    "image": "orig/IMG_6527-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6553-X5.jpg",
+    "image": "orig/IMG_6553-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6564-2-X4.jpg",
+    "image": "orig/IMG_6564-2-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6566-X5.jpg",
+    "image": "orig/IMG_6566-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6590-X4.jpg",
+    "image": "orig/IMG_6590-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6654-X4.jpg",
+    "image": "orig/IMG_6654-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6678-X4.jpg",
+    "image": "orig/IMG_6678-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6710-X5.jpg",
+    "image": "orig/IMG_6710-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6722-X5.jpg",
+    "image": "orig/IMG_6722-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_6909-X5.jpg",
+    "image": "orig/IMG_6909-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_7270-X5.jpg",
+    "image": "orig/IMG_7270-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_7585-X5.jpg",
+    "image": "orig/IMG_7585-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_7634-X5.jpg",
+    "image": "orig/IMG_7634-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8045-2-X4.jpg",
+    "image": "orig/IMG_8045-2-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8123-X4.jpg",
+    "image": "orig/IMG_8123-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8133.jpg",
+    "image": "orig/IMG_8133.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8151-X4.jpg",
+    "image": "orig/IMG_8151-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8273-X5.jpg",
+    "image": "orig/IMG_8273-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8496-X5.jpg",
+    "image": "orig/IMG_8496-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8545.jpg",
+    "image": "orig/IMG_8545.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8611-X5.jpg",
+    "image": "orig/IMG_8611-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_8866.jpg",
+    "image": "orig/IMG_8866.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9005.jpg",
+    "image": "orig/IMG_9005.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9020.jpg",
+    "image": "orig/IMG_9020.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9021-X5.jpg",
+    "image": "orig/IMG_9021-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9022.jpg",
+    "image": "orig/IMG_9022.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9053-X4.jpg",
+    "image": "orig/IMG_9053-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9103-X5.jpg",
+    "image": "orig/IMG_9103-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9224-X4.jpg",
+    "image": "orig/IMG_9224-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9338-X5.jpg",
+    "image": "orig/IMG_9338-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9343-X4.jpg",
+    "image": "orig/IMG_9343-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9348-X3.jpg",
+    "image": "orig/IMG_9348-X3.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9387-X5.jpg",
+    "image": "orig/IMG_9387-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9489-X3.jpg",
+    "image": "orig/IMG_9489-X3.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9489.jpg",
+    "image": "orig/IMG_9489.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9550.jpg",
+    "image": "orig/IMG_9550.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9631.jpg",
+    "image": "orig/IMG_9631.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9646.jpg",
+    "image": "orig/IMG_9646.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9717.jpg",
+    "image": "orig/IMG_9717.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9764.jpg",
+    "image": "orig/IMG_9764.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9767.jpg",
+    "image": "orig/IMG_9767.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9796-XL.jpg",
+    "image": "orig/IMG_9796-XL.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9796.jpg",
+    "image": "orig/IMG_9796.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9807-X5.jpg",
+    "image": "orig/IMG_9807-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9812.jpg",
+    "image": "orig/IMG_9812.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9835-X5.jpg",
+    "image": "orig/IMG_9835-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9867-X5.jpg",
+    "image": "orig/IMG_9867-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9877.jpg",
+    "image": "orig/IMG_9877.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9890-X4.jpg",
+    "image": "orig/IMG_9890-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9908.jpg",
+    "image": "orig/IMG_9908.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9921.jpg",
+    "image": "orig/IMG_9921.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9941-1.jpg",
+    "image": "orig/IMG_9941-1.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9946-1.jpg",
+    "image": "orig/IMG_9946-1.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_99531-X5.jpg",
+    "image": "orig/IMG_99531-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "IMG_9993.jpg",
+    "image": "orig/IMG_9993.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "OSU2-4K.jpg",
+    "image": "orig/OSU2-4K.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "P8101592.jpg",
+    "image": "orig/P8101592.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "P8121606.jpg",
+    "image": "orig/P8121606.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "fireworks1-X4.jpg",
+    "image": "orig/fireworks1-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "fireworks2.jpg",
+    "image": "orig/fireworks2.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "i-4cZ5TR7-X4.jpg",
+    "image": "orig/i-4cZ5TR7-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "i-89pFmwt-X5.jpg",
+    "image": "orig/i-89pFmwt-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "i-zVLM2Dt-X5.jpg",
+    "image": "orig/i-zVLM2Dt-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_0652-X4.jpg",
+    "image": "orig/img_0652-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_1513.jpg",
+    "image": "orig/img_1513.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_3337-X5.jpg",
+    "image": "orig/img_3337-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_3830-X5.jpg",
+    "image": "orig/img_3830-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_6843-X4.jpg",
+    "image": "orig/img_6843-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_7434.jpg",
+    "image": "orig/img_7434.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_7447.jpg",
+    "image": "orig/img_7447.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_7457.jpg",
+    "image": "orig/img_7457.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_8934-X5.jpg",
+    "image": "orig/img_8934-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_89411-X4.jpg",
+    "image": "orig/img_89411-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_89481-X4.jpg",
+    "image": "orig/img_89481-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_8965.jpg",
+    "image": "orig/img_8965.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_8982-X4.jpg",
+    "image": "orig/img_8982-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "img_9303-X4.jpg",
+    "image": "orig/img_9303-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "p71310002-X4.jpg",
+    "image": "orig/p71310002-X4.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "pc240349-X5.jpg",
+    "image": "orig/pc240349-X5.jpg",
     "title": "TBD",
     "link": ""
   },
 
   {
-    "image": "sunset1-X4.jpg",
+    "image": "orig/sunset1-X4.jpg",
     "title": "TBD",
     "link": ""
   }
